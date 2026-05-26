af740896089b822d58ff2af9d1f9171ab0deed82
Yadavalli Sai Varun <vsai6739@gmail.com>
2026-05-26 22:15:02 +0530

    Initial project setup with Spring Boot backend

---
+++ backend/src/test/java/com/finfactor/mealdb_explorer/MealdbExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finfactor.mealdb_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealdbExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
